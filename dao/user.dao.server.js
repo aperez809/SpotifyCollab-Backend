06910cfa54a06185b1ba075163db3f5ac387cc6b
aperez809 <32823645+aperez809@users.noreply.github.com>
2019-06-17 17:49:04 -0400

    building bones of User model

---
+++ dao/user.dao.server.js
@@ -0,0 +1,30 @@
+const userModel = require('../models/user.model.server');
+
+
+createUser = (req, res) => {
+    
+}
+
+findUserById = (req, res) => {
+    
+}
+
+findAllUsers = (req, res) => {
+    
+}
+
+deleteUser = (req, res) => {
+    
+}
+
+updateUser = (req, res) => {
+    
+}
+
+module.exports = {
+    createUser,
+    findAllUsers,
+    findUserById,
+    deleteUser,
+    updateUser
+}
---
+++ models/user.schema.server.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+const userSchema = mongoose.Schema({
+    username: {type: String, required:true},
+    password: String,
+    firstName: String,
+    lastName: String,
+    currentPartyId: Number,
+    userType: {type: String, enum: ['DJ', 'LISTENER']}
+
+}, {collection: 'user'});
+
+module.exports = userSchema;
