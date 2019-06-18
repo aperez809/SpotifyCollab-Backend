abae7a4e31f7aadba2637f58f3b7a5a32bee65b0
welchdaniel <welch.da@husky.neu.edu>
2019-06-18 17:05:53 -0400

    changed database name

--- db/database.js
+++ db/database.js
@@ -1,7 +1,7 @@
 module.exports = function() {
     const mongoose = require('mongoose');
     const databaseName = "song-request";
-    var connectionString = 'mongodb://heroku_8h6pjv01:q0qecegpcqus48ar9nouqchjrs@ds129651.mlab.com:29651/heroku_8h6pjv01/';
-    connectionString += databaseName;
+    var connectionString = 'mongodb://heroku_8h6pjv01:q0qecegpcqus48ar9nouqchjrs@ds129651.mlab.com:29651/heroku_8h6pjv01';
+    //connectionString += databaseName;
     mongoose.connect(connectionString, { useNewUrlParser: true });
 }
