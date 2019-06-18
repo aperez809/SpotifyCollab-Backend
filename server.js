8682691de98177d8ad27b7028c49d128dd86112d
welchdaniel <welch.da@husky.neu.edu>
2019-06-18 16:44:49 -0400

    pointed db file at heroku mlab mongodb and whitelisted connections from heroku angular

--- server.js
+++ server.js
@@ -30,6 +30,8 @@ partyService(app);
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin",
         "http://localhost:4200");
+    res.header("Access-Control-Allow-Origin",
+        "https://song-request-client-angular.herokuapp.com");
     res.header("Access-Control-Allow-Headers",
         "Origin, X-Requested-With, Content-Type, Accept");
     res.header("Access-Control-Allow-Methods",
