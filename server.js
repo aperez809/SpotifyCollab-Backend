63d0b5c72e6a3e30c7697c5bc34ae93b276fe801
welchdaniel <welch.da@husky.neu.edu>
2019-06-19 15:26:34 -0400

    moved res.headers up in server.jhs

--- server.js
+++ server.js
@@ -5,6 +5,21 @@ const mongoose = require('mongoose');
 
 const app = express();
 
+
+app.use(function(req, res, next) {
+    /*res.header("Access-Control-Allow-Origin",
+        "https://song-request-client-angular.herokuapp.com");*/
+    res.setHeader("Access-Control-Allow-Origin",
+        "http://localhost:4200");
+    res.setHeader("Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept");
+    res.setHeader("Access-Control-Allow-Methods",
+        "GET, POST, PUT, DELETE, OPTIONS");
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+    next();
+ });
+
+
 const MongoStore = require('connect-mongo')(session);
 
 app.use(session({
@@ -27,18 +42,5 @@ const partyService = require('./services/party.service.server');
 partyService(app);
 
 require('./services/session.service.server')(app);
-
-app.use(function(req, res, next) {
-    /*res.header("Access-Control-Allow-Origin",
-        "https://song-request-client-angular.herokuapp.com");*/
-    res.setHeader("Access-Control-Allow-Origin",
-        "http://localhost:4200");
-    res.setHeader("Access-Control-Allow-Headers",
-        "Origin, X-Requested-With, Content-Type, Accept");
-    res.setHeader("Access-Control-Allow-Methods",
-        "GET, POST, PUT, DELETE, OPTIONS");
-    res.setHeader("Access-Control-Allow-Credentials", "true");
-    next();
- });
 
 app.listen(process.env.PORT || 3001);
