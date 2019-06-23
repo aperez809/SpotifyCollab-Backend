5fec09335cf7a0d397dc9cf0766a0106242468fc
welchdaniel <welch.da@husky.neu.edu>
2019-06-22 22:54:11 -0400

    forgot a slash in path

--- services/user.service.server.js
+++ services/user.service.server.js
@@ -55,7 +55,7 @@ module.exports = app => {
     app.get('/api/users', findAllUsers);
     app.get('/api/users/:userId', findUserById);
     app.post('/api/users/login', findUserByCredentials);
-    app.post('api/users/register', findUserByUsername);
+    app.post('/api/users/register', findUserByUsername);
     app.post('/api/users', createUser);
     app.delete('/api/users/:userId', deleteUser);
     app.put('/api/users/:userId', updateUser);
